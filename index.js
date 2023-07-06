22b591022a4b7d2f721840c05798546a21704cb2
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-07-06 23:03:23 +0300

    середнє число

---
+++ index.js
@@ -0,0 +1,7 @@
+var number1 = parseFloat(prompt("Введіть перше число:"));
+var number2 = parseFloat(prompt("Введіть друге число:"));
+var number3 = parseFloat(prompt("Введіть третє число:"));
+
+var average = (number1 + number2 + number3) / 3;
+
+alert("Середнє арифметичне: " + average);
