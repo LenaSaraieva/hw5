8bd6137188ca6625aab42f71ceb8ff4620b6439d
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-07-06 23:32:21 +0300

    in English

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var number1 = parseFloat(prompt("Введіть перше число:"));
-var number2 = parseFloat(prompt("Введіть друге число:"));
-var number3 = parseFloat(prompt("Введіть третє число:"));
+var number1 = parseFloat(prompt("Please, enter the first number:"));
+var number2 = parseFloat(prompt("Please, enter the second number:"));
+var number3 = parseFloat(prompt("Please, enter the third number:"));
 
 var average = (number1 + number2 + number3) / 3;
 
-alert("Середнє арифметичне: " + average);
+alert("Arithmetic average: " + average);
